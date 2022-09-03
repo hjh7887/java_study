0421b80c744162f7bb0d3585c71280b6625110b8
jhha <hjh7887@gmail.com>
2022-09-03 21:18:26 +0900

    github 첫 연동

---
+++ src/chapter06/VariableTest.java
@@ -0,0 +1,10 @@
+package chapter06;
+
+public class VariableTest {
+    public static void main(String[] args) {
+        byte bNum = 127;
+        System.out.println(bNum);
+        long lNum = 12345678900L;
+        System.out.println(lNum);
+    }
+}
